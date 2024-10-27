992e1e45cbd26bd927936e70112cc5afda24b796
Francesco <progitalia@gmail.com>
2024-10-27 14:43:16 +0100

    Aggiornamento del sito

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,81 +1,128 @@
-// @ts-check
+// docusaurus.config.js
+
 import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-  url: 'https://www.impresaitalia.info',
-  baseUrl: '/articoli/build/',
-  organizationName: 'impresaitalia',
-  projectName: 'articoli',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  trailingSlash: true,
-  i18n: {
-    defaultLocale: 'it',
-    locales: ['it'],
-  },
-  
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'), // Sidebar unica
-          routeBasePath: '/', // Puoi cambiarlo se necessario
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
-  themeConfig: {
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-        href: "pathname:///"
-      },
-      items: [
-        {
-          to: '/imprese', // Link alla sezione imprese
-          label: 'Imprese',
-          position: 'left',
-        },
-        {
-          to: '/informatica', // Link alla sezione informatica
-          label: 'Informatica',
-          position: 'left',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        // Altri link
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} ImpresaItalia.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  },
+  title: '?Come far per?',
+  tagline: 'Ogni problema ha una soluzione',
+  favicon: 'img/favicon.ico',
+  url: 'https://www.impresaitalia.info',
+  baseUrl: '/articoli/build/',
+  organizationName: 'impresaitalia',
+  projectName: 'articoli',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  trailingSlash: true,
+  i18n: {
+    defaultLocale: 'it',
+    locales: ['it'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.js',
+          editUrl: undefined,
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'How To Do This?',
+      logo: {
+        alt: 'ImpresaItalia - Come fare per?',
+        src: 'https://www.impresaitalia.info/public_resources/logo.jpg',
+        href: "pathname://",
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'prima',
+          position: 'left',
+          label: 'Imprese',
+        },
+        {
+          type: 'docSidebar', // Cambiato da 'informaticaSidebar' a 'docSidebar'
+          sidebarId: 'seconda', // Assicurati che questo ID corrisponda alla sidebar che hai definito
+          position: 'left',
+          label: 'Informatica',
+        },
+        /*{ to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },*/
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Chi siamo',
+          items: [
+            {
+              label: 'Contattaci',
+              to: 'https://www.impresaitalia.info/contacts.asp',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/ItaliaImpresa',
+            },
+            {
+              label: 'Facebook',
+              href: 'https://www.facebook.com/Impresaitalia-878056638937425',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/impresa-italia',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Login',
+              to: 'https://www.impresaitalia.info/users/login.php',
+            },
+            {
+              label: 'Registrati',
+              href: 'https://www.impresaitalia.info/users/register_company.php',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} ImpresaItalia.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
 };
 
 export default config;
